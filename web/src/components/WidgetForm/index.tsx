4301eb9db7700f9e744f5104e8c74387bc754f35
= <brunacalipin@gmail.com>
2022-05-07 16:44:24 -0300

    web version

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 
-import { CloseButton } from "../CloseButton";
-
 import bugImageUrl from '../../assets/bug.svg'
 import ideaImageUrl from '../../assets/idea.svg'
 import thoughtImageUrl from '../../assets/thought.svg'
@@ -41,14 +39,17 @@ export function WidgetForm() {
     function handleRestartFeedback() { 
         setFeedbackType(null);
     }
-    
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
            {!feedbackType ? (
                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
            ) : (
-               <FeedbackContentStep feedbackType={feedbackType} />
+               <FeedbackContentStep 
+               feedbackType={feedbackType} 
+               onFeedbackRestartRequested={handleRestartFeedback}
+               />
            )}
 
             <footer className="text-xs text-neutral-400">
